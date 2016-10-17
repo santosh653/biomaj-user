19b945131d970e86ff5ec8adff2fff62757e5a33
Olivier Sallou <olivier.sallou@irisa.fr>
2016-10-17 13:31:09 +0200

    add address of consul service

--- biomaj_user/biomaj_user_service.py
+++ biomaj_user/biomaj_user_service.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 def consul_declare(config):
     if config['consul']['host']:
         consul_agent = consul.Consul(host=config['consul']['host'])
-        consul_agent.agent.service.register('biomaj_user', service_id=config['consul']['id'], port=config['web']['port'], tags=['biomaj'])
+        consul_agent.agent.service.register('biomaj_user', service_id=config['consul']['id'], address=config['web']['hostname'], port=config['web']['port'], tags=['biomaj'])
         check = consul.Check.http(url='http://' + config['web']['hostname'] + ':' + str(config['web']['port']) + '/api/user', interval=20)
         consul_agent.agent.check.register(config['consul']['id'] + '_check', check=check, service_id=config['consul']['id'])
 
