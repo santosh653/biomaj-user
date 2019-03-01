cedf254d020fb5d7b1c1f9a9facbcc3f7b173645
Olivier Sallou <olivier.sallou@irisa.fr>
2019-03-01 08:45:18 +0100

    fix traefik prefix

--- biomaj_user/biomaj_user_service.py
+++ biomaj_user/biomaj_user_service.py
@@ -37,9 +37,9 @@ def consul_declare(config):
             tags=[
                 'biomaj',
                 'api',
-                'traefik.backend=biomaj-user',
-                'traefik.frontend.rule=PathPrefix:/api/user',
-                'traefik.enable=true'
+                'traefik-int.backend=biomaj-user',
+                'traefik-int.frontend.rule=PathPrefix:/api/user',
+                'traefik-int.enable=true',
             ]
         )
         check = consul.Check.http(url='http://' + config['web']['hostname'] + ':' + str(config['web']['port']) + '/api/user', interval=20)
